769b61747eb09cb6fde2fb0da90e73d41bc51a3d
himanshub2101 <himanshubaghel0121@gmail.com>
2024-07-23 11:21:27 +0530

    initial commit

---
+++ src/schemas/user/user.schemas.ts
@@ -0,0 +1,29 @@
+//src/user/user.schem.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+
+export type UserDocument = User & Document;
+
+
+@Schema()
+export class User extends Document {
+@Prop({ required: true})
+name : string;
+
+@Prop({ required: true, unique: true})
+email:string;
+
+@Prop({required: true})
+password: string;
+
+@Prop({required: true})
+confirmPassword: string;
+
+@Prop({required: true})
+phone: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+
