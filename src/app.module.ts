6a0987299e0b9685b0c7db72951b346a8f0d0ed7
Pratik <pratikyadav3949@gmail.com>
2024-07-27 11:10:52 +0530

    feat(user) add get and update user profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -12,6 +15,10 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule,
     AuthModule,
+    JwtModule.register({
+      secret: process.env.ACCESS_TOKEN_SECRET || 'sgysdgsy',
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
