4ff8b9f6e1bd0eb1adb6d02c9740c345e343cc7d
Pratik <pratikyadav3949@gmail.com>
2024-08-31 16:13:17 +0530

    feat(admin) add admin functionalities

--- src/admin/assignment/controller/assignment.controller.ts
+++ src/admin/assignment/controller/assignment.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateAssignmentDto } from 'src/dto/admin/assignment/create-assignment.dto';
+import { AssignmentsService } from '../service/assignment.service';
 
 @Controller('assignments')
 export class AssignmentsController {
